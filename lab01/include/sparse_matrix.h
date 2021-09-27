37d5bb9378d3f96f7060a5720e6e070138dc6bdc
Paul Koba <paul.koba@pm.me>
2021-09-27 18:44:15 +0000

    Comments.

--- lab01/include/sparse_matrix.h
+++ lab01/include/sparse_matrix.h
@@ -11,6 +11,7 @@
 template <typename T>
 class sparse_matrix {
 private:
+    /* Used internally to be able to hash std::pair<std::size_t, std::size_t> */
     struct pair_hash {
         template <class T1, class T2>
         [[nodiscard]] constexpr std::size_t operator() (const std::pair<T1, T2> &pair) const noexcept {
@@ -22,28 +23,40 @@ private:
     std::size_t m = 0;
     std::unordered_map<std::pair<std::size_t, std::size_t>, T, pair_hash> data;
 public:
+    /* Use sparse_matrix(std::size_t, std::size_t) with specified matrix dimensions instead. */
     sparse_matrix() = delete;
+
+    /* Creates a sparse matrix with specified dimensions */
     sparse_matrix(std::size_t height, std::size_t width) : n(height), m(width) {}
 
+    /* Returns number of rows in matrix. */
     [[nodiscard]] constexpr std::size_t get_height() const noexcept { return n; }
+
+    /* Returns number of columns in matrix. */
     [[nodiscard]] constexpr std::size_t get_width() const noexcept { return m; }
 
+    /* Returns an element at position (i, j). Throws if invalid index is provided. */
     [[nodiscard]] constexpr T get_element(std::size_t i, std::size_t j) noexcept {
+        if(i >= n || j >= m) throw std::out_of_range("sparse_matrix::set_element(): element doesn't fit into the matrix");
         auto it = data.find({i, j});
         return it == data.end() ? 0 : it->second;
     }
 
+    /* Sets an element at position (i, j) to a specified value. Throws if invalid index is provided. */
     constexpr void set_element(std::size_t i, std::size_t j, T value) {
         if(i >= n || j >= m) throw std::out_of_range("sparse_matrix::set_element(): element doesn't fit into the matrix");
         if(value == 0) return;
         data[{i, j}] = value;
     }
 
+    /* Returns reference to an element at position (i, j). Throws if invalid index is provided. */
     [[nodiscard]] constexpr T& operator[](std::pair<std::size_t, std::size_t> pos) noexcept {
-        if(pos.first >= n || pos.second >= m) throw std::out_of_range("sparse_matrix::operator[](): element doesn't fit into the matrix");
+        if(pos.first >= n || pos.second >= m)
+            throw std::out_of_range("sparse_matrix::operator[](): element doesn't fit into the matrix");
         return data[pos];
     }
 
+    /* Perform matrix addition on two matrices. Throws if matrices have incompatible dimensions. */
     [[nodiscard]] sparse_matrix operator+(const sparse_matrix& other) const {
         if(other.m != m || other.n != n) throw std::invalid_argument("sparse_matrix::operator+(): Matrices have incompatible dimensions.");
 
@@ -69,6 +82,7 @@ public:
         return output;
     }
 
+    /* Perform matrix subtraction on two matrices. Throws if matrices have incompatible dimensions. */
     [[nodiscard]] sparse_matrix operator-(const sparse_matrix& other) const {
         return *this + (-other);
     }
@@ -83,16 +97,20 @@ public:
         return *this;
     }
 
+    /* Returns a matrix after transposition. */
     [[nodiscard]] constexpr sparse_matrix transposed() const noexcept {
         sparse_matrix output(m, n);
         for(const auto& p : data) output.data[{p.first.second, p.first.first}] = p.second;
         return output;
     }
 
+    /* Transpose current matrix. */
     void transpose() noexcept {
         *this = transposed();
     }
 
+    /* Perform matrix multiplication on two matrices. Throws if matrices have incompatible dimensions.
+     * O(width * num_nonzero_elements) complexity */
     sparse_matrix operator*(const sparse_matrix& other) {
         if(m != other.n) throw std::invalid_argument("sparse_matrix::operator*(): Matrices have incompatible dimensions.");
 
@@ -110,6 +128,7 @@ public:
         return output;
     }
 
+    /* Finds an element by its value. O(num_of_nonzero_elements) complexity */
     std::pair<std::size_t, std::size_t> find_by_value(T val) {
         for(const auto& p : data) if(p.second == val) return p.first;
 
